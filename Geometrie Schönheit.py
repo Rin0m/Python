fe5074eb3a66f4c760faadac5c1ad5b47fa69c1f
Rinom0 <mo.ghorbani@gmx.de>
2024-10-03 13:37:51 +0200

    added Geometrie Schönheit.py file

---
+++ Geometrie Schönheit.py
@@ -0,0 +1,28 @@
+import turtle as t
+import colorsys
+
+t.bgcolor('black')
+t.tracer(968)
+
+def draw():
+    h =0
+    n =20
+    t.up()
+    t.goto(0,0)
+    t.down()
+    t.pensize()
+    for i in range(73):
+        c = colorsys.hls_to_rgb(h,1,1)
+        h += 1/n
+        t.color(c)
+        t.fd(10)
+        t.circle(i,4.5)
+        for j in range(550):
+            t.lt(971)
+            t.circle(i*.1,j,steps=2)
+            t.circle(i,2)
+
+t.ht()
+draw() 
+t.done()
+        
